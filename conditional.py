8e5c53000d1a5321489ed2612dbcf4d2b9419366
Varun-poluka <poluka.var4126@gmail.com>
2025-07-04 01:37:07 +0530

    text files

--- conditional.py
+++
@@ -1,9 +0,0 @@
-num = int(input("Enter a number between 0-100:"))#taking input and converting to integer
-
-#checking all conditions using if, elif, else
-if num > 50:
-    print("greater than 50")
-elif num < 50:
-    print("less than 50")
-else:
-    print("equal to 50")
