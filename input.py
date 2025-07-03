2b9ae72fef8d63450964593d391ffefc71baf983
Varun-poluka <poluka.var4126@gmail.com>
2025-07-03 22:59:22 +0530

    comments

--- input.py
+++ input.py
@@ -1,2 +1,2 @@
-name = input("what is your name:")#taking input from the user
+name = input("what is your name:")#taking input from the user , returns a string
 print(f"Hello,{name}")#using format string to print using user input
