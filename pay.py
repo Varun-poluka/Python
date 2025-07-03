bb33c00aa91dcae75cd1deebc25a1249a9a2119d
Varun-poluka <poluka.var4126@gmail.com>
2025-07-03 22:59:58 +0530

    conversions

---
+++ pay.py
@@ -0,0 +1,5 @@
+#taking input of both hours and rate and convert input from string to float
+hrs = float(input("Enter the hours worked:"))
+rate = float(input("Enter the rate per hour:"))
+pay = hrs * rate #calculalting the pay
+print(f"pay: {pay}")
