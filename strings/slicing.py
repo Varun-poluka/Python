7f84b3ad47acf2e36907ff297c06abcbf0690d7d
Varun-poluka <poluka.var4126@gmail.com>
2025-07-04 14:52:49 +0530

    slicing

---
+++ strings/slicing.py
@@ -0,0 +1,5 @@
+string = "Monty python"
+print(string[2:5]) # print letter from letter in index 2 to index 4
+print(string[:8]) # print letters until index 8
+print(string[2:]) # print all letter from index 2
+print(string[:]) # print all of the string
