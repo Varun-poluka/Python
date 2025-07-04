d685f71d1051babaf58633a28f68ed4eb1b9fe0a
Varun-poluka <poluka.var4126@gmail.com>
2025-07-05 02:03:15 +0530

    dictionaries

--- dictionaries/get.py
+++ dictionaries/get.py
@@ -5,3 +5,13 @@ words = line.split() # list of words returns from split
 for word in words:
     counts[word] = counts.get(word , 0) + 1 # using get function to create dict of words and count
 print(counts)
+print()
+
+keys = list(counts) #list function return the list of keys from the dictionary
+print(keys)
+print()
+keys = counts.keys() # keys method return a list of dict_keys
+print(keys)
+print()
+values = counts.values() # values method return a list of values in the dictionary
+print(values)
