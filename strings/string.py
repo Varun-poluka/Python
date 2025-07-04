8ce8cf4ab1c6c8fe41fd6e7452935ff80671bc02
Varun-poluka <poluka.var4126@gmail.com>
2025-07-04 14:48:00 +0530

    for loops

--- strings/string.py
+++ strings/string.py
@@ -5,5 +5,5 @@ while index < lenght:
     letter = string[index] # each index in the string represents a letter to loop through
     print(f"{index} {letter}")
     index += 1 # incrementing the index
-for letter in string:
+for letter in string: # printing the letter using a for loop
     print(letter)
