b500ba0844e3b93ae8dc1e3984e8e23a4b5d68bf
Varun-poluka <poluka.var4126@gmail.com>
2025-07-05 00:57:24 +0530

    slicing and building

---
+++ lists/listbuild.py
@@ -0,0 +1,9 @@
+# creating a list from scratch
+lst = list()
+lenght = int(input("enter the lenght of the list:"))
+n = 0
+while n < lenght:
+    element = input(f"Enter the {n} element:")
+    lst.append(element) # append is a method which add a new element to end of the list
+    n += 1
+print(lst)
---
+++ lists/slicing.py
@@ -0,0 +1,6 @@
+lst = [1,2,3,4,5,6,7,8,9]
+# slicing in lists works the same as strings
+print(lst[1:5])
+print(lst[5:])
+print(lst[:7])
+print(lst[:])
