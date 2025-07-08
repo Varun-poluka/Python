36fbd3d7af43d6652d305a400d6711a34f5d41a1
Varun-poluka <poluka.var4126@gmail.com>
2025-07-08 18:18:37 +0530

    types of print

---
+++ print.py
@@ -0,0 +1,9 @@
+# three ways to print a variable containing a value
+
+name = input("enter your name:")
+
+print("Hello " + name) # concatenation space should be given if wanted 
+
+print("Hello" , name) # space is given by default(comma gives the space)
+
+print(f"Hello {name}")# f indicates formal string 
