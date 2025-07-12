4393d03cb8d1b880be471f4be2d6839f5d814b0c
Varun-poluka <poluka.var4126@gmail.com>
2025-07-12 14:29:40 +0530

    :comments

--- python-web/sollution1.py
+++ python-web/sollution1.py
@@ -1,13 +1,15 @@
 import urllib.request , urllib.parse , urllib.error
 from bs4 import BeautifulSoup # type: ignore
 
-url = input("enter the url:")
-html = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(html , "html.parser")
+url = input("enter the url:") # asking the user for the URL 
+html = urllib.request.urlopen(url).read()# opening the url copying into a file and reading the file to give one string
+soup = BeautifulSoup(html , "html.parser")# parsing the string using beautiful soup , return a object 
 
 sum = 0
-spans = soup("span")
+spans = soup("span") # gives a list of spans in the url 
 for span in spans:
-    sum += int(span.contents[0])
+    sum += int(span.contents[0]) # each span has a number in its contents
 
 print(sum)
+
+#http://py4e-data.dr-chuck.net/comments_2250376.html
--- python-web/sollution2.py
+++ python-web/sollution2.py
@@ -1,19 +1,17 @@
 import urllib.request , urllib.parse , urllib.error
 from bs4 import BeautifulSoup # type: ignore
 
-url = input("enter the url:")
-count = int(input("Enter count:"))
-position = int(input("enter position:"))
+url = input("enter the url:")# asking user for the url 
+count = int(input("Enter count:")) # asking the use the number of pages to open
+position = int(input("enter position:")) # asking the user of position of link in each page 
 print(url)
-while (count > 0):
-    html = urllib.request.urlopen(url).read()
-    soup = BeautifulSoup(html , "html.parser")
+while (count > 0): # looping for the number of time pages should be open
+    html = urllib.request.urlopen(url).read()# getting the contents of the page as one big string
+    soup = BeautifulSoup(html , "html.parser") # parsing the html file
 
-    tags = soup("a")
-    print(tags[position-1].get("href" , None))
-    url = tags[position-1].get("href" , None)
+    tags = soup("a") # searching a anchor tags in the page and getting a list
+    print(tags[position-1].get("href" , None)) # printing the url in the position 
+    url = tags[position-1].get("href" , None) # updating the url to the url in the position to open it 
     count -= 1
 
-        
-
-
+#http://py4e-data.dr-chuck.net/known_by_Suhaira.html
